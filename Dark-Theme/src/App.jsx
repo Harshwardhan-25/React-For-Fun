db1d3e4f632db7a8396a8385dd46c4127335dc17
harshwardhan111 <hardhwardhanzade5744@gmail.com>
2024-09-04 23:31:27 +0530

    Dark-Mode

---
+++ Dark-Theme/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  // State to manage the main div's background color
+  const [color, setColor] = useState('white');
+
+  // Function to toggle the main div's background color between black and white
+  const changeBg = () => {
+    setColor((changeBgColor) => (changeBgColor === 'black' ? 'white' : 'black'));
+  };
+
+  return (
+    <div className="w-full h-screen duration-200" style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-16 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg px-3 py-2 rounded-2xl" style={{backgroundColor:color === 'black' ? 'white':'black'}}>
+          <button
+            onClick={changeBg}
+            className=""
+            // Conditional button styling based on the main div's background color
+            style={{
+              backgroundColor: color === 'black' ? 'white' : 'black',
+              color: color === 'black' ? 'black' : 'white',
+            }}
+          >
+            {color === 'black' ? 'Switch to White' : 'Switch to Black'}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
